a4608a4b9d00bb7818982a8925fb53d5cab0600b
Hello-Qasim <Qasimjaved625@gmail.com>
2023-03-25 14:05:15 +0000

    Bug in remove button

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,12 +1,33 @@
 import React from "react";
 import classes from "./Cart.module.css";
 import Window from "../GUI/Window";
+import CartItem from "./CartItem";
+import { useContext } from "react";
+import { NameContext } from "../../Vaulet/CartProvider";
 
 function Cart(props) {
+  const cartCtx = useContext(NameContext);
+
+  const totalAmount = `£${cartCtx.totalAmount.toFixed(2)}`;
+  const withItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (list) => {
+    cartCtx.addItem({ ...list, amount: 1 });
+  };
+
   const cartlists = (
     <ul className={classes.cartItems}>
-      {[{ id: "a1", name: "Tikka", amount: 3, price: 10 }].map((list) => (
-        <li>{list.name}</li>
+      {cartCtx.items.map((list) => (
+        <CartItem
+          key={list.id}
+          name={list.name}
+          amount={list.amount}
+          price={list.price}
+          onRemove={cartItemRemoveHandler.bind(null, list.id)}
+          onAdd={cartItemAddHandler.bind(null, list)}
+        />
       ))}
     </ul>
   );
@@ -16,13 +37,13 @@ function Cart(props) {
       {cartlists}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>40.40</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes.buttonAlt} onClick={props.onCartHide}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {withItems && <button className={classes.button}>Order</button>}
       </div>
     </Window>
   );
