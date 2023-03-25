d3e16dc8cedecf4f5bff5a6c417cf36fd0b073f3
Qas <qasimjaved625@gmail.com>
2023-03-25 20:41:40 +0000

    react-app

--- src/Components/Design/HeaderButton.js
+++ src/Components/Design/HeaderButton.js
@@ -1,18 +1,37 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import classes from "./HeaderButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import { NameContext } from "../../Vaulet/CartProvider";
 
 function CartButton(props) {
+  const [btnIsColoured, setBtnIsColoured] = useState(false);
   const cartCtx = useContext(NameContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsColoured ? classes.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsColoured(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsColoured(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClickCart}>
+    <button className={btnClasses} onClick={props.onClickCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
