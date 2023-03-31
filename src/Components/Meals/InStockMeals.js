1cd975e7f9328e530a42c8b3415b0772571dab6e
Qas <qasimjaved625@gmail.com>
2023-03-31 20:32:52 +0100

    react-app

--- src/Components/Meals/InStockMeals.js
+++ src/Components/Meals/InStockMeals.js
@@ -1,46 +1,51 @@
+import { useEffect, useState } from "react";
 import React from "react";
 import classes from "./InStockMeals.module.css";
 import Card from "../GUI/Card";
 import MealsList from "./MealsList";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Chicken-Tikka",
-    description: "Scatter over spring onions to serve",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Chicken shish kebabs",
-    description: "These kebabs are big on flavour!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Hot wings",
-    description: "The sticky maple and butter glaze on these wings",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Honey Chicken",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
-const mealsList = DUMMY_MEALS.map((meal) => (
-  <MealsList
-    id={meal.id}
-    key={meal.id}
-    name={meal.name}
-    description={meal.description}
-    price={meal.price}
-  />
-));
-
 function InStockMeals() {
+  const [meals, setmeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://food-app-48409-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setmeals(loadedMeals);
+      setIsLoading(false);
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealsList
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  if (isLoading) {
+    return (
+      <section className={classes.loading}>
+        <p>Loading....</p>
+      </section>
+    );
+  }
+
   return (
     <section className={classes.meals}>
       <Card>
